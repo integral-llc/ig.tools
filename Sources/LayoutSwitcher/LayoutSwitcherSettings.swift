96abb91b6372348020f6b8c0ce5ac19f1da300e7
Eugene Rata <eugen.rata@gmail.com>
2026-02-26 10:51:49 -0600

    Set default opacity to 1.0 for TextShortcuts and LayoutSwitcher windows

--- Sources/LayoutSwitcher/LayoutSwitcherSettings.swift
+++ Sources/LayoutSwitcher/LayoutSwitcherSettings.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct LayoutSwitcherSettings: ToolSettings {
-    var opacity: Double = 0.75
+    var opacity: Double = 1.0
     var alwaysOnTop: Bool = true
     var playSoundOnSwitch: Bool = true
     var minWordLength: Int = 3
--- Sources/TextShortcuts/TextShortcutsSettings.swift
+++ Sources/TextShortcuts/TextShortcutsSettings.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct TextShortcutsSettings: ToolSettings {
-    var opacity: Double = 0.75
+    var opacity: Double = 1.0
     var alwaysOnTop: Bool = true
     var playSoundOnExpansion: Bool = true
 }
